8512ab3057b9bc0824c3a9b9e67a68c079f141be
jwidmer74 <jakron74@hotmail.com>
2017-10-07 21:44:38 -0700

    round 2

--- Heights2/src/heights2/Heights2.java
+++ Heights2/src/heights2/Heights2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package heights2;
-
+import java.util.Scanner;
 /**
  *
  * @author Mr Porcupine
@@ -15,7 +15,33 @@ public class Heights2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int numStudents;
+        double [] heights;
+        System.out.println("How many students do you have?");
+        numStudents = in.nextInt();
+        heights = new double [numStudents];
+        for (int i = 0; i < numStudents; i++)
+        {
+          System.out.println("Enter height: " );
+          heights [i] = in.nextDouble();
+        }
+        double maxHeight = heights[0];
+        for (int i = 1; i < heights.length; i++)
+        {
+          if (maxHeight < heights [i])
+              maxHeight = heights [i];
+        }
+        double total = 0;
+        for (int i =0; i < heights.length; i++)
+        {
+            total += heights[i];
+        }
+        System.out.println("The tallest student is: " + maxHeight + " inches");
+        System.out.println("The average height for all students is : " + (total/numStudents)
+        + " inches");
+        
     }
+    
     
 }
